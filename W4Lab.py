89e8d244c58c4bc83782e2ac97dce8c009be8801
Ava Weiner <avaw@umich.edu>
2017-11-05 14:45:16 -0500

    add files

--- W4Lab.py
+++
@@ -1,8 +0,0 @@
-fname = "mbox-short.txt"
-f = open(fname, "r")
-for line in f.readlines():
-    if "From" in line:
-        print (line)
-        name = line.split()[1]
-        n = name.split("@")[0]
-        print (n)
